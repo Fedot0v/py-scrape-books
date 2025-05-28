0cf5870e401fa1b59b9777eb9fe06fd79681967c
nikit <nikita.fedotov222@gmail.com>
2025-05-28 14:35:53 +0300

    solution

---
+++ scrapy_books/scrapy_books/pipelines.py
@@ -0,0 +1,15 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+
+
+class ScrapyBooksPipeline:
+    def process_item(self, item, spider):
+        item["price"] = float(item["price"].replace("£", "").strip())
+        item["rating"] = item["rating"].count(".icon-star")
+        return item
---
+++ scrapy_books/scrapy_books/spiders/quotes_spider.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+
+import scrapy
+from scrapy.http import Response
+from scrapy_books.items import ScrapyBooksItem
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+    start_urls = [" https://books.toscrape.com/"]
+
+    def parse(self, response: Response, **kwargs):
+        product_links = response.css(".product_pod h3 a::attr(href)").getall()
+
+        for link in product_links:
+            yield response.follow(link, callback=self.parse_product_details)
+
+        next_page = response.css(".next a::attr(href)").get()
+        if next_page:
+            yield response.follow(next_page, callback=self.parse)
+
+    def parse_product_details(self, response: Response, **kwargs):
+        item = ScrapyBooksItem()
+        item["title"] = response.css(".product_main h1::text").get()
+        item["price"] = response.css(
+            ".price_color::text"
+        ).get().replace("£", "")
+        item["amount_in_stock"] = response.css(
+            ".instock.availability::text"
+        ).re_first(r"\d+")
+        item["rating"] = response.css(
+            ".star-rating::attr(class)"
+        ).re_first(r"star-rating (\w+)")
+        item["category"] = response.css(
+            ".breadcrumb li:nth-child(3) a::text"
+        ).get()
+        item["description"] = response.css(
+            "#product_description + p::text"
+        ).get()
+        item["upc"] = response.xpath(
+            "//table[@class='table table-striped']\
+                //tr[th[text()='UPC']]/td/text()"
+        ).get()
+        yield item
